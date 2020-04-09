90be7ef05b2c9c52893cc490d5ffdd5678c8d6fc
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2020-04-09 16:06:32 +0300

    minor bugfix

--- src/repositories/CurrencyRepository.java
+++ src/repositories/CurrencyRepository.java
@@ -10,7 +10,6 @@ public class CurrencyRepository {
 
     private CurrencyRepository() {
         currencies.add(BaseCurrency.getInstance());
-        currencies.add(new Currency("Romanian New Leu", "RON"));
         currencies.add(new Currency("United States Dollar", "USD"));
         currencies.add(new Currency("British Pound", "GBP"));
         currencies.add(new Currency("Euro", "EUR"));
